635f3af594cdb3d4204be71bc1ed4e5b642edd70
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-26 12:19:43 +0530

    ✅ House Robber | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-26

--- medium/0198-house-robber/solution.java
+++ medium/0198-house-robber/solution.java
@@ -1,18 +1,3 @@
-class Solution {
-    public int theft(int ind,int[] nums,int[] dp){
-        if(ind == 0){
-            return nums[0];
-        }
-
-        if(ind < 0){
-            return 0;
-        }
-
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
-
-        int pick = nums[ind] + theft(ind - 2,nums,dp);
         int notpick = theft(ind -1,nums,dp);
         return dp[ind] = Math.max(pick,notpick);
     }
