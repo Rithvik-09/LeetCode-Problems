4495bc35a2044850100ad336d7da2e8a7cf63d78
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-26 16:11:34 +0530

    ✅ Isomorphic Strings | Easy | Java | Runtime: 5 ms (98.72%) | Memory: 44.2 MB | 2026-08-26

--- easy/0205-isomorphic-strings/solution.java
+++ easy/0205-isomorphic-strings/solution.java
@@ -1,14 +1,19 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] sm = new int[256];
+        int[] tm = new int[256];
 
-        for(int i = 0;i<s.length();i++){
-            char a = s.charAt(i);
-            char b = t.charAt(i);
+        for(int i = 0;i<s.length();i++){
+            char a = s.charAt(i);
+            char b = t.charAt(i);
 
-            if(sm[a] != tm[b]){
-                return false;
-            }
+            if(sm[a] != tm[b]){
+                return false;
+            }
 
-            sm[a] = i+1;
-            tm[b] = i+1;
-        }
-    }
-        return true;
+            sm[a] = i+1;
+            tm[b] = i+1;
+        }
+        return true;
+    }
+}
