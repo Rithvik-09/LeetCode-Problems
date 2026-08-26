6e5fe27b529fc26ff3686c643adccb835a93c2f9
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-26 15:35:35 +0530

    ✅ Majority Element II | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-26

---
+++ medium/0229-majority-element-ii/solution.java
@@ -0,0 +1,14 @@
+
+            if(hm.get(nums[i])== mini){
+                res.add(nums[i]);
+            }
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        for(int i=0;i<n;i++){
+
+        int mini = n / 3 + 1;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+            if(res.size() == 2) break;
+        }
+        return res;
+    }
