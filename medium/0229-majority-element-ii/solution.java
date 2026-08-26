7966a99725cab9e0c566fd631ef9110dabe542f5
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-26 15:35:40 +0530

    ✅ Majority Element II | Medium | Java | Runtime: 20 ms (8.97%) | Memory: 53.2 MB | 2026-08-26

--- medium/0229-majority-element-ii/solution.java
+++ medium/0229-majority-element-ii/solution.java
@@ -1,14 +1,20 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
 
-            if(hm.get(nums[i])== mini){
-                res.add(nums[i]);
-            }
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        for(int i=0;i<n;i++){
+        List<Integer> res = new ArrayList<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int mini = n / 3 + 1;
 
-        int mini = n / 3 + 1;
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
 
-            if(res.size() == 2) break;
-        }
-        return res;
-    }
+            if(hm.get(nums[i])== mini){
+                res.add(nums[i]);
+            }
+
+            if(res.size() == 2) break;
+        }
+        return res;
+    }
+}
