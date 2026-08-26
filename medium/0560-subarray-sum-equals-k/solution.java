f4885e3ae4b0f367d75fab2840c1c2d61e3afd99
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-26 15:28:33 +0530

    ✅ Subarray Sum Equals K | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-26

---
+++ medium/0560-subarray-sum-equals-k/solution.java
@@ -0,0 +1,14 @@
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                sum += nums[j];
+            }
+
+            int sum = 0;
+
+                if(sum == k){
+                    count++;
+                }
+        }
+    }
+        return count;
+}
