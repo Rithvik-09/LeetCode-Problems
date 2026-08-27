cf996a7944b03b9e4741346ff4d92ccda4d073fe
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-27 17:54:19 +0530

    ✅ Largest Number At Least Twice of Others | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.5 MB | 2026-08-27

--- easy/0747-largest-number-at-least-twice-of-others/solution.java
+++ easy/0747-largest-number-at-least-twice-of-others/solution.java
@@ -1,11 +1,22 @@
-        }   
-            }else if(nums[i]>secondLargest){
-                secondLargest = nums[i];
-            }
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int largest = -1;
+        int secondLargest = -1;
+        int ind = -1;
 
-        if(largest >= 2* secondLargest){
-            return ind;
-        } 
-        return -1;
-    }
+        for(int i =0;i<nums.length;i++){
+            if(nums[i] >largest){
+                secondLargest = largest;
+                largest = nums[i];
+                ind = i;
+            }else if(nums[i]>secondLargest){
+                secondLargest = nums[i];
+            }
+        }   
+
+        if(largest >= 2* secondLargest){
+            return ind;
+        } 
+        return -1;
+    }
 }
