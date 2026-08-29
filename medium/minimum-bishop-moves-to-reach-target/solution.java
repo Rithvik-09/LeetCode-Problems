b43f0a1e38c2cd5264ef316716cdc0bd5077c9cc
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-29 21:01:06 +0530

    ✅ Q1. Minimum Bishop Moves to Reach Target | Medium | Java | Runtime: 0 ms | Memory: 43 MB | 2026-08-29

--- medium/minimum-bishop-moves-to-reach-target/solution.java
+++ medium/minimum-bishop-moves-to-reach-target/solution.java
@@ -1,1 +1,19 @@
-source.length == target.length == 2
+class Solution {
+    public int minBishopMoves(int[] source, int[] target) {
+        int sr = source[0];
+        int sc = source[1];
+
+        int tr = source[0];
+        int tc = source[1];
+
+        if(sr == tr && sc == tc){
+            return 0;
+        }
+
+        if(Math.abs(sr-tr) == Math.abs(sc-tc)){
+            return 1;
+        }
+
+        return -1;
+    }
+}
