13cc04f60f17096b4b45af011b8fae85f5904d6d
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-29 21:24:16 +0530

    ✅ Q3. Lexicographically Largest String After Pair Transformations | Medium | Java | Runtime: 78 ms (33.33%) | Memory: 133.8 MB | 2026-08-29

---
+++ medium/4366-lexicographically-largest-string-after-pair-transformations/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String[] largestString(int[] nums) {
+        String[] ans = new String[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            int x = nums[i];
+
+            StringBuilder sb = new StringBuilder();
+
+            int zcount  = x/(1<<25);
+            x = x %(1<<25);
+            
+            for(int j =0;j<zcount;j++){
+                sb.append('z');
+            }
+
+            for(int bit =24;bit>=0;bit--){
+                if((x & (1<<bit)) != 0){
+                    sb.append((char)('a' + bit));
+                }
+            }
+
+            ans[i] = sb.toString();
+        }
+        return ans;
+    }
+}
