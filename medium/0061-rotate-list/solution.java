22557719409db2581ad006c6196f462bfbcaef89
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-08-31 19:42:31 +0530

    ✅ Rotate List | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 44.2 MB | 2026-08-31

--- medium/0061-rotate-list/solution.java
+++ medium/0061-rotate-list/solution.java
@@ -1,14 +1,36 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
 
-        if (k == 0) {
-            return head;
-        }
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
 
-        tail.next = head;
+        int n = 1;
+        ListNode tail = head;
 
-        int nth = n - k - 1;
-        ListNode last = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            n++;
+        }
 
-        for (int i = 0; i < nth; i++) {
-            last = last.next;
-        }
+        k = k % n;
 
+        if (k == 0) {
+            return head;
+        }
+
+        tail.next = head;
+
+        int nth = n - k - 1;
+        ListNode last = head;
+
+        for (int i = 0; i < nth; i++) {
+            last = last.next;
+        }
+
+        ListNode newHead = last.next;
+        last.next = null;
+
+        return newHead;
+    }
+}
