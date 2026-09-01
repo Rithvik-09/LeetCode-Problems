457b3d2f6b8f4f2f359e199def2d0bc1a795b0c5
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-09-01 18:33:47 +0530

    ✅ Most Common Word | Easy | Java | Runtime: 18 ms (35.73%) | Memory: 45 MB | 2026-09-01

--- easy/0819-most-common-word/solution.java
+++ easy/0819-most-common-word/solution.java
@@ -1,15 +1,34 @@
+import java.util.*;
 
-        for (String word : hm.keySet()) {
-            if (answer.equals("") || hm.get(word) > hm.get(answer)) {
-                answer = word;
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
 
-        String answer = "";
-        }
-                hm.put(s, hm.getOrDefault(s, 0) + 1);
-            }
+        paragraph = paragraph.toLowerCase();
+        paragraph = paragraph.replaceAll("[^a-z]", " ");
 
-        for (String s : str) {
-            if (!ban.contains(s)) {
-        }
-            ban.add(b.toLowerCase());
-        for (String b : banned) {
+        String str[] = paragraph.split("\\s+");
+
+        HashMap<String, Integer> hm = new HashMap<>();
+        Set<String> ban = new HashSet<>();
+
+        for (String b : banned) {
+            ban.add(b.toLowerCase());
+        }
+
+        for (String s : str) {
+            if (!ban.contains(s)) {
+                hm.put(s, hm.getOrDefault(s, 0) + 1);
+            }
+        }
+
+        String answer = "";
+
+        for (String word : hm.keySet()) {
+            if (answer.equals("") || hm.get(word) > hm.get(answer)) {
+                answer = word;
+            }
+        }
+
+        return answer;
+    }
+}
