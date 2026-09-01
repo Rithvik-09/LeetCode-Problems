809dde3103b8dbbdd9d00e9fe1261bd0d1dfaa89
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-09-01 18:30:58 +0530

    ✅ Most Common Word | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-09-01

---
+++ easy/0819-most-common-word/solution.java
@@ -0,0 +1,14 @@
+            if(!ban.contains(s)){
+
+        for(String s : str){
+        for(String b : banned){
+            ban.add(b);
+        }
+        Set<String> ban = new HashSet<>();
+
+
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        String str[] = paragraph.split(" ");
+    public String mostCommonWord(String paragraph, String[] banned) {
+class Solution {
