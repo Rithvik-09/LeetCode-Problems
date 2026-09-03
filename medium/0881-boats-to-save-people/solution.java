14e8ae5ea7670b0f2fe679b6bb42d9dfd4177e71
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-09-04 00:31:55 +0530

    ✅ Boats to Save People | Medium | Java | Runtime: 20 ms (83.21%) | Memory: 56.2 MB | 2026-09-03

--- medium/0881-boats-to-save-people/solution.java
+++ medium/0881-boats-to-save-people/solution.java
@@ -1,14 +1,21 @@
-        int right = n-1;
-        while(left <= right) {
-            if(people[left] + people[right] <= limit) {
-                boats++; 
-            } else {
-                right--; 
-                left++;
-                right--; 
-                boats++;
-            }
-        }
-        
-    }
-        return boats; 
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int n = people.length; 
+        int boats = 0; 
+        int left = 0; 
+        int right = n-1;
+        while(left <= right) {
+            if(people[left] + people[right] <= limit) {
+                boats++; 
+                right--; 
+                left++;
+            } else {
+                right--; 
+                boats++;
+            }
+        }
+        return boats; 
+        
+    }
+}
