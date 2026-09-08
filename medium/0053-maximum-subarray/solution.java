d209c665d9b7fdf0face37b7317d240a91fd01dc
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-09-08 07:22:01 +0530

    ✅ Maximum Subarray | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-09-08

---
+++ medium/0053-maximum-subarray/solution.java
@@ -0,0 +1,14 @@
+            maxSum = Math.max(maxSum, currentSum);
+
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+            currentSum += nums[i];
+        for(int i = 0; i < nums.length; i++) {
+        int currentSum = 0;
+
+        int maxSum = nums[0];
+    public int maxSubArray(int[] nums) {
+class Solution {
