9084fce5d853d9a2fe2f78594b3b074cf12a34a1
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-09-10 21:07:29 +0530

    ✅ Swap Nodes in Pairs | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.2 MB | 2026-09-10

--- medium/0024-swap-nodes-in-pairs/solution.java
+++ medium/0024-swap-nodes-in-pairs/solution.java
@@ -1,14 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
 
-            first.next = second.next;
-            second.next = first;
-            prev.next = second;
+        dummy.next = head;
 
-            prev = first;
-        }
-        
+        ListNode prev = dummy;
 
-        return dummy.next;
-    }
+        while(prev.next != null && prev.next.next != null){
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+        
+
+        return dummy.next;
+    }
 }
-            ListNode second = prev.next.next;
-            ListNode first = prev.next;
