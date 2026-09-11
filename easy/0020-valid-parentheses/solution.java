e9ae6a88d29cf8c6284cf5b449c1aaf079a08bf5
Rithvik Gouru <rithvikgouru09@gmail.com>
2026-09-11 17:22:05 +0530

    ✅ Valid Parentheses | Easy | Java | Runtime: 4 ms (36.32%) | Memory: 43.2 MB | 2026-09-11

--- easy/0020-valid-parentheses/solution.java
+++ easy/0020-valid-parentheses/solution.java
@@ -1,13 +1,25 @@
-                    return false;
-                }
-                char open = st.peek();
-                if((ch == ')' && open != '(') || (ch == '}' && open != '{') || (ch == ']' && 
-                open != '[')){
-                    return false;
-                }
-                st.pop();
-            }
-        }
-                return st.isEmpty();
-    }
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch =='{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char open = st.peek();
+                if((ch == ')' && open != '(') || (ch == '}' && open != '{') || (ch == ']' && open != '[')){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+                return st.isEmpty();
+    }
 }
